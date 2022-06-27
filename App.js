8342380f1a2551af697b4052179e73b67fa5d170
Wez-Young <oldwezyoung@gmail.com>
2022-06-27 15:43:37 +0100

    updated add goal functionality

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+import GoalItem  from './components/goal_item';
+import GoalInput from './components/goal_input';
 
 export default function App() {
 
-  const [inputtedGoalText, setInputtedGoalText] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  function inputGoalHandler(inputtedText) {
-    setInputtedGoalText(inputtedText);
-  }
-  function addGoalHandler() {
+  function addGoalHandler(inputtedGoalText) {
     setCourseGoals((currentGoals) => [
       ...currentGoals,
       {
@@ -21,22 +19,12 @@ export default function App() {
 
   return (
     <View style={styles.rootContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.goalInput}
-          placeholder="Input a goal"
-          onChangeText={inputGoalHandler}
-        />
-        <Button title="Add Goal" onPress={addGoalHandler} />
-      </View>
+     <GoalInput AddGoal={addGoalHandler}/>
       <View style={styles.listContainer}>
+
         <FlatList data={courseGoals}
         renderItem={itemData => {
-          return (
-            <View elevation={5} style={styles.listItem}>
-              <Text style={styles.listItemText}>{itemData.item.text}</Text>
-            </View>
-          );
+            return <GoalItem text={itemData.item.text}/>;
         }}
         alwaysBounceVertical={false}/>
       </View>
@@ -50,40 +38,10 @@ const styles = StyleSheet.create({
     paddingTop: 60,
     paddingHorizontal: 14,
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-  },
-  goalInput: {
-    borderWidth: 1,
-    borderColor: '#4e4e4e9c',
-    marginRight: 10,
-    padding: 8,
-    width: '70%',
-  },
   listContainer: {
     flex: 6,
     alignItems: 'center',
     width: '100%',
-  },
-  listItem: {
-    backgroundColor: 'white',
-    margin: 8,
-    borderRadius: 8,
-    padding: 5,
-    alignItems: 'center',
-    shadowOffset: { width: 2, height: 3},
-  shadowColor: 'black',
-  shadowOpacity: 0.3,
-  elevation: 3,
-  },
-  listItemText: {
-
   },
   scrollContainer: {
     width: '90%'
